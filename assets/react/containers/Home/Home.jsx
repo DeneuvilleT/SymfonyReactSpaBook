3f07a0d489045c833059c5e83b9d05ad81a10c94
DeneuvilleT <deneuville.thomas@gmail.com>
2024-03-01 00:01:52 +0100

    wip - Début du travail sur les fiches du gite

--- assets/react/containers/Home/Home.jsx
+++ assets/react/containers/Home/Home.jsx
@@ -18,29 +18,36 @@ const Home = () => {
         {/* Afficher les locations */}
         {locations.length !== 0 ? (
           <aside className={styles.locations}>
-            <h2>Locations trouvées :</h2>
             <ul>
               {locations.map((location) => (
                 <>
-                  <li
-                    key={location.id}
-                    style={{ backgroundImage: `url('${window.location.origin}/uploads/images/${location.cottage.covers[0].path}')` }}
-                  >
-                    <h3>{location.cottage.name}</h3>
-                    <p dangerouslySetInnerHTML={{ __html: location.cottage.description }} />
-                  </li>
+                  <li key={location.id}>
+                    <div className={styles.cardsLoc}>
+                      <div
+                        className={styles.imgBox}
+                        style={{ backgroundImage: `url('${window.location.origin}/uploads/images/${location.cottage.covers[0].path}')` }}
+                      ></div>
 
-                  {location.cottage.covers.length !== 0 ? (
-                    <ul>
-                      {location.cottage.covers.map((cover) => (
-                        <li key={cover.id}>
-                          <img src={`${window.location.origin}/uploads/images/${cover.path}`} alt={`Photo ${location.cottage.name}`} />{" "}
-                        </li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <></>
-                  )}
+                      <div className={styles.content}>
+                        <span>
+                          <h3>{location.cottage.name}</h3>
+                        </span>
+                        <p dangerouslySetInnerHTML={{ __html: location.cottage.description }} />
+                      </div>
+                    </div>
+
+                    {location.cottage.covers.length !== 0 ? (
+                      <ul className={styles.thumbails}>
+                        {location.cottage.covers.map((cover) => (
+                          <li key={cover.id}>
+                            <img src={`${window.location.origin}/uploads/images/${cover.path}`} alt={`Photo ${location.cottage.name}`} />{" "}
+                          </li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <></>
+                    )}
+                  </li>
                 </>
               ))}
             </ul>
