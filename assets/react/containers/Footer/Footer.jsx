1a2f8d7a07fcaf5016ebcb93f35578baa1f0a440
DeneuvilleT <deneuville.thomas@gmail.com>
2024-03-18 22:15:16 +0100

    upd - Footer fini

--- assets/react/containers/Footer/Footer.jsx
+++ assets/react/containers/Footer/Footer.jsx
@@ -7,15 +7,17 @@ const Footer = () => {
   return (
     <>
       <div className={styles.reinsurance}>
-        <h3>
-          Trouvez<br />nous
-        </h3>
 
-        <address>
-          Azur Gardens <br />
-          4, rue Chez Chabot <br />
-          17460 RIOUX
-        </address>
+        <div>
+          <h3>
+            Trouvez<br />nous
+          </h3>
+
+          <address>
+            Azur Gardens, 4 Rue Chez Chabot <br />
+            17460 RIOUX
+          </address>
+        </div>
 
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2788.733631479978!2d-0.7031512870695933!3d45.656170720487346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480106b7cc0a6ec5%3A0x2e47e8dea565f34e!2sAZUR%20GARDENS!5e0!3m2!1sfr!2sfr!4v1709745045453!5m2!1sfr!2sfr"
@@ -28,7 +30,7 @@ const Footer = () => {
         <div className={styles.footerContainer}>
           <div className={styles.footerSecAboutUs}>
             <h2>A propos</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque unde voluptate reprehenderit aut eos quibusdam.</p>
+            <p>Retrouvez-nous sur les différents <br /> réseaux sociaux</p>
             <ul className={styles.footerSocialLinks}>
               <li><Link to={"/"}><Icon icon="icomoon-free:instagram" width="2em" height="2em" style={{ color: 'black' }} /></Link></li>
               <li><Link to={"/"}><Icon icon="icomoon-free:facebook2" width="2em" height="2em" style={{ color: 'black' }} /></Link></li>
@@ -38,16 +40,16 @@ const Footer = () => {
             <h2>Support</h2>
             <ul>
               <li><Link to={"/"}>Mentions légales</Link></li>
-              <li><Link to={"/"}>CDGV</Link></li>
+              <li><Link to={"/"}>CGV</Link></li>
               <li><Link to={"/"}>Moyens de paiement</Link></li>
             </ul>
           </div>
           <div className={styles.footerQuickLinks}>
             <h2>Site</h2>
             <ul>
-              <li><Link to={"/"}>Connexion</Link></li>
-              <li><Link to={"/"}>Inscription</Link></li>
-              <li><Link to={"/"}>Mon compte</Link></li>
+              <li><Link to={"/login"}>Connexion</Link></li>
+              <li><Link to={"/register"}>Inscription</Link></li>
+              <li><Link to={"/profile"}>Mon compte</Link></li>
               <li><Link to={"/"}>Accueil</Link></li>
             </ul>
           </div>
@@ -62,7 +64,7 @@ const Footer = () => {
       </footer>
 
       <div className={styles.footerCopyrightText}>
-        <p><span>Copyright&nbsp;<Icon icon="la:copyright" width="1.2em" height="1.2em" style={{ color: '#555' }} />&nbsp;2024</span><span>PAUSE ET NATURE</span><span>Tous droits réservés</span></p>
+        <p><span>Copyright&nbsp;<Icon icon="la:copyright" width="1.2em" height="1.2em" style={{ color: '#555' }} />&nbsp;2024</span><span>Pause et nature</span><span>Tous droits réservés</span></p>
       </div>
     </>
   );
